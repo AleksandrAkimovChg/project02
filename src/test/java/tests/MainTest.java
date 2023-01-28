4a52569dee8035940823878556385fb55af51fe4
Aleksandr Akimov <gtwood@yandex.ru>
2023-01-28 17:16:27 +0300

    test commit

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -1,8 +1,19 @@
 package tests;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class MainTest extends BaseTest {
 
+    @Test
+    public void testBaseURLLoadedSuccessfully() {
+        final String expectedUrl = getBaseUrl();
 
+        String actualUrl =
+                openBaseURL()
+                        .getURL();
+
+        Assert.assertEquals(actualUrl, expectedUrl);
+    }
 }
