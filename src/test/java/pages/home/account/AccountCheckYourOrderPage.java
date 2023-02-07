d912dffb0580fa00400208014fd7432c0ef263e0
Anastasia Yakimova <anastasiayy@yahoo.com>
2023-02-07 14:31:09 -0500

    Update AccountCheckYourOrderPage.java

--- src/test/java/pages/home/account/AccountCheckYourOrderPage.java
+++ src/test/java/pages/home/account/AccountCheckYourOrderPage.java
@@ -3,7 +3,7 @@ package pages.home.account;
 import org.openqa.selenium.WebDriver;
 import pages.base_abstract.FormPage;
 
-public class AccountCheckYourOrderPage extends FormPage {
+public class AccountCheckYourOrderPage extends TablePage {
     public AccountCheckYourOrderPage(WebDriver driver) {
         super(driver);
     }
