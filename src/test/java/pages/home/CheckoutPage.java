93fdc5d2cfb9460b9b1410b5e78e3d6bbddf6abd
Nataliia Oliver <psvnatali@gmail.com>
2023-02-07 12:45:54 -0600

    Changed CheckoutPage inheritance to extends from TablePage

--- src/test/java/pages/home/CheckoutPage.java
+++ src/test/java/pages/home/CheckoutPage.java
@@ -1,9 +1,9 @@
 package pages.home;
 
 import org.openqa.selenium.WebDriver;
-import pages.base_abstract.FormPage;
+import pages.base_abstract.TablePage;
 
-public class CheckoutPage extends FormPage {
+public class CheckoutPage extends TablePage {
     public CheckoutPage(WebDriver driver) {
         super(driver);
     }
