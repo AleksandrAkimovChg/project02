691be80c295a4649c8bdafe915f9fac5bb4a22a4
verafes <verafes@gmail.com>
2023-02-07 18:41:40 -0800

    testLoginMenuCustomerText_changesAfterLogin() added (#13)
    Thank you all

--- src/test/java/tests/home/account/AccountLoginTest.java
+++ src/test/java/tests/home/account/AccountLoginTest.java
@@ -1,7 +1,28 @@
 package tests.home.account;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.home.HomePage;
 
 public class AccountLoginTest extends BaseTest {
 
+    @Test
+    public void testLoginMenuCustomerText_changesAfterLogin() {
+        final String expectedLoginMenuText = "Welcome back Test";
+
+        HomePage homePage = new HomePage(getDriver());
+
+        String oldLoginMenuText = openBaseURL().getLoginCustomerText();
+
+        homePage.clickLoginCustomerMenu()
+                .clickClearInputRegularUserLogin("testtestoff940")
+                .clickClearInputRegularUserPassword("Testoff29012003")
+                .clickLoginButton();
+
+        String actualLoginMenuText = homePage.getLoginCustomerText();
+
+        Assert.assertNotEquals(oldLoginMenuText, actualLoginMenuText);
+        Assert.assertEquals(actualLoginMenuText, expectedLoginMenuText);
+    }
 }
