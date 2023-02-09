d02d88ad41dec0cbb2b58386af5eb64c3bc4d337
Anastasia Yakimova <anastasiayy@yahoo.com>
2023-02-09 15:30:39 -0500

    add testLoginWithValidCredentials_NavigatesToMyAccountPage() (#16)

--- src/test/java/tests/my_account/MyAccountTest.java
+++ src/test/java/tests/my_account/MyAccountTest.java
@@ -1,6 +1,34 @@
 package tests.my_account;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.my_account.MyAccountPage;
 
 public class MyAccountTest extends BaseTest {
+
+    @Test
+    public void testLoginWithValidCredentials_NavigatesToMyAccountPage() {
+        final String expectedURL = "https://automationteststore.com/index.php?rt=account/account";
+        final String expectedTitle = "My Account";
+
+        MyAccountPage myAccountPage = new MyAccountPage(getDriver());
+
+        String oldURL = openBaseURL().getURL();
+        String actualURL =
+                openBaseURL()
+                        .clickAccountMenu()
+                        .clickLoginAccountSubmenu()
+                        .clickClearInputRegularUserLogin("testtestoff940")
+                        .clickClearInputRegularUserPassword("Testoff29012003")
+                        .clickLoginButton()
+                        .getURL();
+
+        Assert.assertNotEquals(oldURL, actualURL);
+
+        String actualTitle = myAccountPage.getTitle();
+
+        Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
 }
