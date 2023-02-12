69e0fa07742b52128b30e3dc6abf5339590ba621
maksymiasko <maksymiasko@gmail.com>
2023-02-12 19:18:20 +0000

    added testAPIAllLinksAreNotBrokenMen_ID58_Pagee

--- src/test/java/pages/men/MenPage.java
+++ src/test/java/pages/men/MenPage.java
@@ -1,10 +1,23 @@
 package pages.men;
 
 import org.openqa.selenium.WebDriver;
-import pages.base_abstract.ProductCategoriesPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.base_abstract.*;
+
+import java.util.List;
 
 public class MenPage extends ProductCategoriesPage {
+
+    @FindBy(xpath = "//div[@class='subcategories']//a")
+    private List<WebElement> menMenuList;
+
     public MenPage(WebDriver driver) {
         super(driver);
+    }
+
+    public List<WebElement> getMenMenuList() {
+
+        return this.menMenuList;
     }
 }
--- src/test/java/tests/men/MenTest.java
+++ src/test/java/tests/men/MenTest.java
@@ -1,9 +1,16 @@
 package tests.men;
 
 import base.BaseTest;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import pages.men.MenPage;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 import static testData.ProjectConstants.*;
 
@@ -16,5 +23,49 @@ public class MenTest extends BaseTest {
 
         Assert.assertEquals(menPage.getURL(), MEN_PAGE_URL);
         Assert.assertEquals(menPage.getTitle(), MEN_PAGE_TITLE);
+    }
+
+    @Test
+    public void testAPIAllLinksAreNotBrokenMen_ID58_Page() {
+        String linkURL = "";
+        int responseCode;
+        int actualWorkingLinksCount = 0;
+
+        MenPage menPage = openBaseURL().clickMenMenu();
+
+        List<WebElement> linksList = menPage.getMenMenuList();
+
+        final int expectedWorkingLinksCount = linksList.size();
+        int internalLinks = expectedWorkingLinksCount;
+
+        for (WebElement link : linksList) {
+            linkURL = link.getAttribute("href");
+
+            if (linkURL != null && !linkURL.isBlank() && !linkURL.isEmpty()) {
+                if (!linkURL.startsWith(getBaseUrl())) {
+                    Reporter.log(linkURL + " is externalLink ", true);
+                    internalLinks--;
+                } else {
+                    try {
+                        HttpURLConnection connection = (HttpURLConnection) (new URL(linkURL).openConnection());
+                        connection.setRequestMethod("HEAD");
+                        connection.connect();
+
+                        responseCode = connection.getResponseCode();
+
+                        if (responseCode == 200) {
+                            actualWorkingLinksCount++;
+                        } else {
+                            Reporter.log(linkURL + " is broken, responseCode " + responseCode, true);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        Assert.assertEquals(menPage.getURL(), MEN_PAGE_URL);
+        Assert.assertEquals(actualWorkingLinksCount, internalLinks);
+        Assert.assertEquals(actualWorkingLinksCount, expectedWorkingLinksCount);
     }
 }
