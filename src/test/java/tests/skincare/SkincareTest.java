fba181b1ab99e16e3abbc2894633202c01d32030
Natallia S <happylady1806@gmail.com>
2023-02-13 19:41:47 -0500

    testSkincareLink_NavigatesTo_SkincarePage() added

--- src/test/java/tests/skincare/SkincareTest.java
+++ src/test/java/tests/skincare/SkincareTest.java
@@ -1,6 +1,16 @@
 package tests.skincare;
 
 import base.BaseTest;
+import org.testng.annotations.Test;
+import pages.skincare.SkincarePage;
 
 public class SkincareTest extends BaseTest {
+
+    @Test
+    public void testSkincareLink_NavigatesTo_SkincarePage() {
+
+//        SkincarePage skincarePage =
+//                openBaseURL()
+//                        .clickSkincareMenu();
+    }
 }
