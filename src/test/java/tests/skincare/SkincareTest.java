c8b371b4b1b914afc3548ee663861543b2746068
Natallia S <happylady1806@gmail.com>
2023-02-15 16:56:03 -0500

    [AT][Home][TopMenu][Skincare] testSkincareLink_NavigatesTo_SkincarePage() <NatalliaOwl> (#32)
    testSkincareLink_NavigatesTo_SkincarePage() added

--- src/test/java/tests/skincare/SkincareTest.java
+++ src/test/java/tests/skincare/SkincareTest.java
@@ -1,6 +1,28 @@
 package tests.skincare;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.home.HomePage;
+import pages.skincare.SkincarePage;
+
+import static testData.ProjectConstants.SKINCARE_PAGE_TITLE;
+import static testData.ProjectConstants.SKINCARE_PAGE_URL;
 
 public class SkincareTest extends BaseTest {
+
+    @Test
+    public void testSkincareLink_NavigatesTo_SkincarePage() {
+
+        SkincarePage  skincarePage = new SkincarePage(getDriver());
+
+        String oldURL = openBaseURL()
+                        .getURL();
+
+        new HomePage(getDriver()).clickSkincareMenu();
+
+        Assert.assertNotEquals(oldURL, getDriver().getCurrentUrl());
+        Assert.assertEquals(skincarePage.getURL(), SKINCARE_PAGE_URL);
+        Assert.assertEquals(skincarePage.getTitle(), SKINCARE_PAGE_TITLE);
+    }
 }
