83b2b63c1fdefb555ea11d976443312c02edfb5f
verafes <verafes@gmail.com>
2023-02-23 13:55:31 -0800

    testClickLeftNavTopCartLink_NavigatesToCart added

--- src/test/java/tests/home/cart/CartTest.java
+++ src/test/java/tests/home/cart/CartTest.java
@@ -1,7 +1,25 @@
 package tests.home.cart;
 
 import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.home.cart.CartPage;
+
+import static testData.ProjectConstants.CART_PAGE_URL;
 
 public class CartTest extends BaseTest {
 
+    @Test
+    public void testClickLeftNavTopCartLink_NavigatesToCart() {
+        final String expectedTitle = "Shopping Cart";
+        final String expectedURL = CART_PAGE_URL;
+
+        String oldURL = openBaseURL().getURL();
+
+        CartPage cartPage = openBaseURL().clickLeftNavTopCartMenu();
+
+        Assert.assertNotEquals(oldURL, getDriver().getCurrentUrl());
+        Assert.assertEquals(cartPage.getURL(), expectedURL);
+        Assert.assertEquals(cartPage.getTitle(), expectedTitle);
+    }
 }
