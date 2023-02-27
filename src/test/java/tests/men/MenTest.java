884d72f86330972b398ce5b9c218ae0f078898be
maksymiasko <maksymiasko@gmail.com>
2023-02-27 15:51:16 +0000

    refactoring testSearchProductByName

--- src/test/java/tests/men/MenTest.java
+++ src/test/java/tests/men/MenTest.java
@@ -11,6 +11,8 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static testData.ProjectConstants.*;
 
@@ -68,4 +70,50 @@ public class MenTest extends BaseTest {
         Assert.assertEquals(actualWorkingLinksCount, internalLinks);
         Assert.assertEquals(actualWorkingLinksCount, expectedWorkingLinksCount);
     }
+
+    @Test
+    public void testAPIAllLinksAreNotBrokenMenStream_ID58_Page() {
+        MenPage menPage = openBaseURL().clickMenMenu();
+        List<WebElement> linksList = menPage.getMenMenuList();
+
+        int expectedWorkingLinksCount = linksList.size();
+
+        Map<Boolean, List<String>> linksByInternalOrExternal = linksList.stream()
+                .map(link -> link.getAttribute("href"))
+                .filter(linkURL -> linkURL != null && !linkURL.isBlank() && !linkURL.isEmpty())
+                .collect(Collectors.partitioningBy(linkURL -> linkURL.startsWith(getBaseUrl())));
+
+        List<String> externalLinks = linksByInternalOrExternal.get(false);
+        List<String> internalLinks = linksByInternalOrExternal.get(true);
+
+        int internalLinksCount = internalLinks.size();
+        int actualWorkingLinksCount = (int) internalLinks.stream()
+                .map(linkURL -> {
+                    try {
+                        HttpURLConnection connection = (HttpURLConnection) (new URL(linkURL).openConnection());
+                        connection.setRequestMethod("HEAD");
+                        connection.connect();
+
+                        int responseCode = connection.getResponseCode();
+
+                        if (responseCode == 200) {
+                            return true;
+                        } else {
+                            Reporter.log(linkURL + " is broken, responseCode " + responseCode, true);
+                            return false;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return false;
+                    }
+                })
+                .filter(isWorking -> isWorking)
+                .count();
+
+        Assert.assertEquals(menPage.getURL(), MEN_PAGE_URL, "The Men page URL is as expected");
+        Assert.assertEquals(actualWorkingLinksCount, internalLinksCount, "Number of internal working links is as expected");
+        Assert.assertEquals(actualWorkingLinksCount, expectedWorkingLinksCount, "Number of working links is as expected");
+        Assert.assertEquals(externalLinks.size(), 0, "External links are present in the Men page menu");
+    }
 }
+
