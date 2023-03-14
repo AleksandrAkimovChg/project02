449f0710df6f3f5d8e04710254e46a5775d3987e
verafes <verafes@gmail.com>
2023-03-14 16:11:34 -0700

    testLoginLogoutSubmenuText added (#86)
    Thank you all!

--- src/test/java/tests/my_account/MyAccountTest.java
+++ src/test/java/tests/my_account/MyAccountTest.java
@@ -3,6 +3,7 @@ package tests.my_account;
 import base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.home.HomePage;
 import pages.my_account.MyAccountPage;
 
 import static testData.ProjectConstants.*;
@@ -32,5 +33,28 @@ public class MyAccountTest extends BaseTest {
 
         Assert.assertEquals(actualURL, expectedURL);
         Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    @Test
+    public void testLoginSubmenuText_changesToLogoutAfterLogin() {
+        final String expectedLoginMenuText = " LOGIN";
+        final String expectedLogoutMenuText = "LOGOUT";
+
+        String oldLoginMenuText = openBaseURL()
+                .mouseHoverAccountTopMenu()
+                .getAccountLoginSubmenuText();
+
+        HomePage homePage = new HomePage(getDriver());
+
+        String actualLogoutMenuText = homePage.clickAccountMenu()
+                .clickLoginAccountSubmenu()
+                .clickClearInputRegularUserLogin(LOGIN_NAME)
+                .clickClearInputRegularUserPassword(PASSWORD)
+                .clickLoginButton()
+                .mouseHoverAccountTopMenu()
+                .getAccountLogoutSubmenuText();
+
+        Assert.assertEquals(oldLoginMenuText, expectedLoginMenuText);
+        Assert.assertEquals(actualLogoutMenuText, expectedLogoutMenuText);
     }
 }
