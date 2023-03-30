28393fedf38aaa95e32e9f502100da3c4e8d9f38
verafes <verafes@gmail.com>
2023-03-29 20:35:41 -0700

    testMyAccountSideMenuLinks_NavigateToCorrespondingPages added

--- src/test/java/testData/TestData.java
+++ src/test/java/testData/TestData.java
@@ -66,4 +66,32 @@ public class TestData {
                         "https://automationteststore.com/index.php?rt=account/login", "Account Login"}
         };
     }
+
+    @DataProvider(name = "MyAccountSideMenuList")
+    public static Object[][] myAccountSideMenuList() {
+
+        return new Object[][] {
+                {0, "\u00A0 Account Dashboard", "https://automationteststore.com/index.php?rt=account/account",
+                        "https://automationteststore.com/index.php?rt=account/account", "MY ACCOUNT", "My Account"},
+                {1, "\u00A0 My wish list", "https://automationteststore.com/index.php?rt=account/wishlist",
+                        "https://automationteststore.com/index.php?rt=account/wishlist", "MY WISH LIST", "My wish list"},
+                {2, "\u00A0 Edit account details", "https://automationteststore.com/index.php?rt=account/edit",
+                        "https://automationteststore.com/index.php?rt=account/edit", "MY ACCOUNT INFORMATION", "My Account Information"},
+                {3, "\u00A0 Change password", "https://automationteststore.com/index.php?rt=account/password",
+                        "https://automationteststore.com/index.php?rt=account/password", "CHANGE PASSWORD", "Change Password"},
+                {4, "\u00A0 Manage Address Book", "https://automationteststore.com/index.php?rt=account/address",
+                        "https://automationteststore.com/index.php?rt=account/address", "ADDRESS BOOK", "Address Book"},
+                {5, "\u00A0 Order history", "https://automationteststore.com/index.php?rt=account/history",
+                        "https://automationteststore.com/index.php?rt=account/history", "MY ORDER HISTORY", "My Order History"},
+                {6, "\u00A0 Transaction history", "https://automationteststore.com/index.php?rt=account/transactions",
+                        "https://automationteststore.com/index.php?rt=account/transactions", "MY TRANSACTIONS", "My Transactions"},
+                {7, "\u00A0 Downloads", "https://automationteststore.com/index.php?rt=account/download",
+                        "https://automationteststore.com/index.php?rt=account/download", "ACCOUNT DOWNLOADS", "Account Downloads"},
+                {8, "\u00A0 Notifications", "https://automationteststore.com/index.php?rt=account/notification",
+                        "https://automationteststore.com/index.php?rt=account/notification", "NOTIFICATIONS AND NEWSLETTER",
+                        "Notifications and Newsletter"},
+                {9, "\u00A0 Logoff", "https://automationteststore.com/index.php?rt=account/logout",
+                        "https://automationteststore.com/index.php?rt=account/logout", "ACCOUNT LOGOUT", "Account Logout"}
+        };
+    }
 }
--- src/test/java/tests/my_account/MyAccountTest.java
+++ src/test/java/tests/my_account/MyAccountTest.java
@@ -1,10 +1,14 @@
 package tests.my_account;
 
 import base.BaseTest;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.home.HomePage;
 import pages.my_account.MyAccountPage;
+import testData.TestData;
+
+import java.util.List;
 
 import static testData.ProjectConstants.*;
 
@@ -77,4 +81,42 @@ public class MyAccountTest extends BaseTest {
         Assert.assertEquals(actualLoginMenuText, expectedLoginMenuText);
     }
 
+    @Test(dataProviderClass = TestData.class, dataProvider = "MyAccountSideMenuList")
+    public void testMyAccountSideMenuLinks_NavigateToCorrespondingPages(
+            int index, String menuText, String href, String url, String header, String title) {
+
+        MyAccountPage myAccountPage = openBaseURL()
+                .clickAccountMenu()
+                .clickLoginAccountSubmenu()
+                .clickClearInputRegularUserLogin(LOGIN_NAME)
+                .clickClearInputRegularUserPassword(PASSWORD)
+                .clickLoginButton();
+
+        List<WebElement> myAccountSideMenu = myAccountPage.getMyAccountSideMenuLinks();
+
+        String oldUrl = myAccountPage.getURL();
+        String oldHeader = myAccountPage.getH1HeaderText();
+
+        String actualHref = myAccountPage.getMyAccountSideMenuHrefByIndex(index);
+        String actualMenuText = myAccountPage.getMyAccountSideMenuTextByIndex(index);
+
+        String actualUrl = myAccountPage.clickMenu(index, myAccountSideMenu).getURL();
+        String actualH1Header = myAccountPage.getH1HeaderText();
+        String actualTitle = myAccountPage.getTitle();
+
+        if (index < 1) {
+            Assert.assertEquals(actualUrl, oldUrl);
+            Assert.assertEquals(actualH1Header, oldHeader);
+        } else {
+            Assert.assertNotEquals(actualUrl, oldUrl);
+            Assert.assertNotEquals(actualH1Header, oldHeader);
+        }
+
+        Assert.assertEquals(actualMenuText, menuText);
+        Assert.assertEquals(actualHref, href);
+
+        Assert.assertEquals(actualUrl, url);
+        Assert.assertEquals(actualH1Header, header);
+        Assert.assertEquals(actualTitle, title);
+    }
 }
