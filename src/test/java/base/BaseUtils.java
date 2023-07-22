3911e1e48ce9729f43aca561f7e13879ec995bf5
Aleksandr Akimov <alexander.akimov.chg@gmail.com>
2023-07-22 21:24:36 +0300

    fix

--- src/test/java/base/BaseUtils.java
+++ src/test/java/base/BaseUtils.java
@@ -1,7 +1,6 @@
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -63,57 +62,30 @@ public final class BaseUtils {
         WebDriverManager.chromedriver().setup();
     }
 
-    static Properties getProperties() {
-        return properties;
-    }
-
     static boolean isServerRun() {
         return System.getenv("CI_RUN") != null;
     }
-
-//    static WebDriver createDriver() {
-//        WebDriver driver = new ChromeDriver(chromeOptions);
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        return driver;
-//    }
 
     static public WebDriver createDriver() throws IOException {
         String browserName;
         if(isServerRun()) {
             browserName = "chrome";
-
         } else {
-
             Properties prop = new Properties();
             FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
                     + "/src/test/resources/local.properties");
             prop.load(fis);
-
             browserName = prop.getProperty("browser");
         }
 
-        //properties class
-//        Properties prop = new Properties();
-//        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
-//                + "/src/test/resources/local.properties");
-//        prop.load(fis);
-//        //если мавен задает переменную тогда она, если нет то берет из проперти файла
-//        String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") : prop.getProperty("browser");
-        //считывает прерменную из проперти файла
-
         if(browserName.contains("chrome")) {
-//            ChromeOptions options = new ChromeOptions();
             WebDriverManager.chromedriver().setup();
-//            if (browserName.contains("headless")) {
-//                options.addArguments("headless");
-//            }
+            if (browserName.contains("headless")) {
+                chromeOptions.addArguments("headless");
+            }
             driver = new ChromeDriver(chromeOptions);
-//            driver = new ChromeDriver(options);
-//            driver.manage().window().setSize(new Dimension(1440, 900));  //help run full screen
         }
         else if (browserName.equalsIgnoreCase("firefox")) {
-            //firefox
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
         }
